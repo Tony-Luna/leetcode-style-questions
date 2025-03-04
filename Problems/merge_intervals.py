f6a305912489cd513334cb3b754805d88dbdfc94
Tony Luna <anluna1998@gmail.com>
2025-03-04 00:48:27 -0800

    Updated questions 2 and 3

--- Problems/merge_intervals.py
+++ Problems/merge_intervals.py
@@ -1,8 +1,35 @@
 # Script: merge_intervals.py
 """
 Question: Merge Intervals
-Given an array of intervals where `intervals[i] = [start_i, end_i]`, merge all overlapping intervals and return an array of the non-overlapping intervals that cover all the intervals in the input.
+Given an array of intervals where `intervals[i] = [start_i, end_i]`, 
+merge all overlapping intervals and return an array of 
+the non-overlapping intervals that cover all the intervals in the input.
 Example:
     Input: intervals = [[1,3], [2,6], [8,10], [15,18]]
     Output: [[1,6], [8,10], [15,18]]
 """
+
+def merge_intervals(intervals):
+    if not intervals:
+        return []
+
+    # Sort the intervals by their starting point.
+    intervals.sort(key=lambda x: x[0])
+    
+    merged = [intervals[0]]
+    
+    for current in intervals[1:]:
+        last_merged = merged[-1]
+        # If the current interval overlaps with the last merged interval, merge them.
+        if current[0] <= last_merged[1]:
+            last_merged[1] = max(last_merged[1], current[1])
+        else:
+            # Otherwise, add the current interval as is.
+            merged.append(current)
+    
+    return merged
+
+if __name__ == "__main__":
+    intervals = [[1,3], [2,6], [8,10], [15,18]]
+    merged = merge_intervals(intervals)
+    print("Merged intervals:", merged)
--- Problems/product_except_self.py
+++ Problems/product_except_self.py
@@ -1,9 +1,35 @@
 # Script: product_except_self.py
 """
 Question: Product of Array Except Self
-Given an integer array `nums`, return an array `answer` such that `answer[i]` is equal to the product of all the elements of `nums` except `nums[i]`. 
+Given an integer array `nums`, return an array `answer` 
+such that `answer[i]` is equal to the product of all the 
+elements of `nums` except `nums[i]`. 
 Solve without using division and in O(n) time.
 Example:
     Input: nums = [1, 2, 3, 4]
     Output: [24, 12, 8, 6]
 """
+
+def product_array_except_self(nums):
+    length = len(nums)
+    # Initialize the answer array with 1's
+    answer = [1] * length
+
+    # Calculate left products
+    left = 1
+    for i in range(length):
+        answer[i] = left
+        left *= nums[i]
+
+    # Multiply with right products
+    right = 1
+    for i in range(length - 1, -1, -1):
+        answer[i] *= right
+        right *= nums[i]
+
+    return answer
+
+if __name__ == "__main__":
+    nums = [1, 2, 3, 4]
+    answer = product_array_except_self(nums)
+    print(answer)
